4ea680e1042ea6ca03d50830e8cbbdfc4dd62647
ArkSayCode <fangbing21cn@gmail.com>
2024-06-23 23:18:40 -0700

    The Beggining
    edited mod.js and layers.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	name: "The Question Tree",
+	id: "TQT",
+	author: "ArkSayCode",
+	pointsName: "thoughts",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,16 +13,15 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "The Beggining of Consciousness",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
-	<h3>v0.0</h3><br>
-		- Added things.<br>
-		- Added stuff.`
+	<h3>v0.1</h3><br>
+		- ???.<br>`
 
-let winText = `Congratulations! You have reached the end and beaten this game, but for now...`
+let winText = `Congratulations! You have reached the end and beaten this game, or have you???`
 
 // If you add new functions anywhere inside of a layer, and those functions have an effect when called, add them here.
 // (The ones here are examples, all official functions are already taken care of)
